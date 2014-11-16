f9b3e5f71d9e449a580ef488ac06e6332308e694
Melvin Lammerts <melvinl141@gmail.com>
2014-11-16 01:07:10 +0100

    More T-swizzle

--- app/database/seeds/RecordTableSeeder.php
+++ app/database/seeds/RecordTableSeeder.php
@@ -1,13 +1,36 @@
 <?php
-class RecordTableSeeder extends Seeder {
-	public function run() {
-		DB::table('records')->delete();
+class RecordTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('records')->delete();
 
-		Record::create(array(
-			'artist' => 'Taylor Swift',
-			'title' => '1989',
-			'year' => '2014',
-			'user_id' => 1,
-		));
-	}
+        Record::create(array(
+            'artist' => 'Taylor Swift',
+            'title' => '1989',
+            'year' => '2014',
+            'user_id' => 1,
+        ));
+
+        Record::create(array(
+            'artist' => 'Taylor Swift',
+            'title' => 'Red',
+            'year' => '2012',
+            'user_id' => 1,
+        ));
+
+        Record::create(array(
+            'artist' => 'Taylor Swift',
+            'title' => 'Speak Now',
+            'year' => '2010',
+            'user_id' => 1,
+        ));
+
+        Record::create(array(
+            'artist' => 'Taylor Swift',
+            'title' => 'Fearless - Platinum Edition',
+            'year' => '2009',
+            'user_id' => 1,
+        ));
+    }
 }
--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,12 +1,14 @@
 <?php
-class UserTableSeeder extends Seeder {
-	public function run() {
-		DB::table('users')->delete();
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->delete();
 
-		User::create(array(
-			'id' => 1,
-			'email' => 'demo@demo.local',
-			'password' => Hash::make('demo!'),
-		));
-	}
+        User::create(array(
+            'id' => 1,
+            'email' => 'demo@demo.local',
+            'password' => Hash::make('demo!'),
+        ));
+    }
 }
