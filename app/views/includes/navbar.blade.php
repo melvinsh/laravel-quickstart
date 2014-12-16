e97e7ffd8986376f2150f6a78f7e3366a701aaec
Melvin Lammerts <melvin@melvin.sh>
2014-12-16 10:14:24 +0100

    Probably faster

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -9,13 +9,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{{ route('get_dashboard') }}}">Laravel Quickstart</a>
+      <a class="navbar-brand" href="{{ route('get_dashboard') }}">Laravel Quickstart</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       @if (Auth::check())
         <ul class="nav navbar-nav">
-          <li><a href="{{{ route('get_dashboard') }}}">Dashboard</a></li>
-          <li><a href="{{{ route('get_records') }}}">Records</a></li>
+          <li><a href="{{ route('get_dashboard') }}">Dashboard</a></li>
+          <li><a href="{{ route('get_records') }}">Records</a></li>
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
@@ -23,19 +23,19 @@
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->email }} <span
                       class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="{{{ route('show_profile', array('id' => Auth::user()->id)) }}}">Profile</a>
+              <li><a href="{{ route('show_profile', array('id' => Auth::user()->id)) }}">Profile</a>
               </li>
-              <li><a href="{{{ route('get_settings') }}}">Settings</a></li>
+              <li><a href="{{ route('get_settings') }}">Settings</a></li>
               <li class="divider"></li>
-              <li><a href="{{{ route('get_logout') }}}">Sign out</a></li>
+              <li><a href="{{ route('get_logout') }}">Sign out</a></li>
             </ul>
           </li>
         </ul>
       @endif
       @if (!Auth::check())
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="{{{ route('get_login') }}}">Sign in</a></li>
-          <li><a href="{{{ route('get_signup') }}}">Sign up</a></li>
+          <li><a href="{{ route('get_login') }}">Sign in</a></li>
+          <li><a href="{{ route('get_signup') }}">Sign up</a></li>
         </ul>
       @endif
     </div>
